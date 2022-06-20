a21e0dca24c75e891d7f7bdac9b6d4b563f0136f
Bryan Bosire <bryanbosire@gmail.com>
2022-06-20 20:06:19 +0300

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  fetch("https://anapioficeandfire.com/api/books")
+  .then(res => res.json() )
+  .then(data => data.forEach(renderBooks(name => renderBooks(name))));
+  console.log(data)
 }
 
 function renderBooks(books) {
